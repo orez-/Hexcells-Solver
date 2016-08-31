8e5da94959a12efd4ac13c41f576a1d15d9a2058
Brian Shaginaw <brian.shaginaw@warbyparker.com>
2016-08-31 01:00:20 -0400

    Fix issues with board reader.
    Also add tests to ensure correctness.

--- tests.py
+++ tests.py
@@ -19,6 +19,21 @@ def _get_bump(line):
     return bool((len(spaces) + bool(char)) % 4 // 2)
 
 
+def _get_hex(text):
+    color = Color.black
+    if text == '-':
+        color = Color.yellow
+    elif text == 'x':
+        text = '-'
+    elif text == 'o':
+        text = '-'
+        color = Color.blue
+    elif 'o' in text:
+        text = text.strip('o')
+        color = Color.blue
+    return Hex(text, color)
+
+
 def _board_from_string(string):
     lines = string.split('\n')
     leading_spaces = min(len(re.match(r'^ *', line).group(0)) for line in lines if line.strip())
@@ -30,27 +45,20 @@ def _board_from_string(string):
         line = line.rstrip()
         hex_line = []
         while line:
-            spaces, item = re.match(r'^( *)(\S+)', line).groups()
-            line = line[len(spaces) + len(item):]
-            spaces = len(spaces) // 4
+            spaces, text = re.match(r'^( *)(\S+)', line).groups()
+            line = line[len(spaces) + len(text):]
+            spaces = (len(spaces) + (len(text) > 1)) // 4
             hex_line += [None] * spaces
-            color = Color.black
-            if item == '-':
-                color = Color.yellow
-            elif item == 'x':
-                item = '-'
-            elif item == 'o':
-                item = '-'
-                color = Color.blue
-
-            hex_line.append(Hex(item, color))
+            hex_line.append(_get_hex(text))
         hex_lines.append(hex_line)
 
     board = HexBoard()
     for y, line in enumerate(hex_lines):
         for x, elem in enumerate(line):
             if elem:
-                board[x * 2 + ((y % 2) ^ bump), y // 2 - x] = elem
+                hx = x * 2 + ((y - bump) % 2)
+                hy = y // 2 - x + ((bump % 2) * (not (hx % 2)))
+                board[hx, hy] = elem
 
     return board
 
@@ -61,6 +69,85 @@ def _split_on_arrow(board_strings):
     left_lines = '\n'.join(line[:arrow] for line in lines)
     right_lines = '\n'.join(line[arrow + 2:] for line in lines)
     return left_lines, right_lines
+
+
+class HexMock:
+    def __init__(self, text, color):
+        self.text = text
+        self.color = color
+
+
+class BoardReaderTest(unittest.TestCase):
+    @classmethod
+    def set_display_fn(cls, fn):
+        cls.display_fn = staticmethod(fn)
+
+    def test_structure1(self):
+        board = _board_from_string("""
+            1
+          2   3
+            4
+          5   6
+            -
+        """)
+        dx, dy, _ = next(coord for coord, hex_ in board._board.items() if hex_.text == '4')
+        disp = '\n{}'.format(self.display_fn(board))
+        mock = HexMock(None, None)
+        assert board.get((dx, dy - 1), mock).text == '1', disp
+        assert board.get((dx - 1, dy), mock).text == '2', disp
+        assert board.get((dx + 1, dy - 1), mock).text == '3', disp
+        assert board.get((dx, dy), mock).text == '4', disp
+        assert board.get((dx - 1, dy + 1), mock).text == '5', disp
+        assert board.get((dx + 1, dy), mock).text == '6', disp
+        assert board.get((dx, dy + 1), mock).color == Color.yellow, disp
+
+    def test_structure2(self):
+        board = _board_from_string("""
+          x
+            1
+          2   3
+            4
+          5   6
+            -
+        """)
+        dx, dy, _ = next(coord for coord, hex_ in board._board.items() if hex_.text == '4')
+        disp = '\n{}'.format(self.display_fn(board))
+        mock = HexMock(None, None)
+        assert board.get((dx - 1, dy - 1), mock).color == Color.black, disp
+        assert board.get((dx, dy - 1), mock).text == '1', disp
+        assert board.get((dx - 1, dy), mock).text == '2', disp
+        assert board.get((dx + 1, dy - 1), mock).text == '3', disp
+        assert board.get((dx, dy), mock).text == '4', disp
+        assert board.get((dx - 1, dy + 1), mock).text == '5', disp
+        assert board.get((dx + 1, dy), mock).text == '6', disp
+        assert board.get((dx, dy + 1), mock).color == Color.yellow, disp
+
+    def test_leader_structure1(self):
+        board = _board_from_string("""
+        3   1
+
+            2
+        """)
+        dx, dy, _ = next(coord for coord, hex_ in board._board.items() if hex_.text == '1')
+        disp = '\n{}'.format(self.display_fn(board))
+        mock = HexMock(None, None)
+        assert board.get((dx - 2, dy + 1), mock).text == '3', disp
+        assert board.get((dx, dy), mock).text == '1', disp
+        assert board.get((dx, dy + 1), mock).text == '2', disp
+
+
+    def test_leader_structure2(self):
+        board = _board_from_string("""
+        3   1
+
+           {2}
+        """)
+        dx, dy, _ = next(coord for coord, hex_ in board._board.items() if hex_.text == '1')
+        disp = '\n{}'.format(self.display_fn(board))
+        mock = HexMock(None, None)
+        assert board.get((dx - 2, dy + 1), mock).text == '3', disp
+        assert board.get((dx, dy), mock).text == '1', disp
+        assert board.get((dx, dy + 1), mock).text == '{2}', disp
 
 
 class SolverUnitTest(unittest.TestCase):
@@ -149,6 +236,24 @@ class SolverUnitTest(unittest.TestCase):
         """)
 
 
+    def test_noncontiguous_no_info(self):
+        self.assertSolve("""
+           x          x
+         -   -      -   -
+          -2-   =>   -2-
+         x   -      x   -
+           -          -
+        """)
+
+    def test_noncontiguous_one_option(self):
+        self.assertSolve("""
+           x          x
+         -   -      o   x
+          -2-   =>   -2-
+         x   o      x   o
+           -          x
+        """)
+
 def run_tests(display_fn):
     loader = unittest.TestLoader()
     SolverUnitTest.set_display_fn(display_fn)
@@ -158,4 +263,5 @@ def run_tests(display_fn):
 
 if __name__ == '__main__':
     SolverUnitTest.set_display_fn(display.display_full_board)
+    BoardReaderTest.set_display_fn(display.display_full_board)
     unittest.main()
